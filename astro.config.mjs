cc7137e9496f10747be2b72e12d41126c133ab01
Johannes Wiebe <johanneswiebe@posteo.de>
2025-09-18 13:18:50 +0100

    Initial commit from template

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  integrations: [tailwind(), mdx(), icon({
+    include: {
+      mdi: ["*"]
+    }
+  })]
+});
